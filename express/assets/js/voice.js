aa007e3aae9f4e89aabd2e69a56e6fe7f4c7f16d
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 04:17:35 +0100

    Luke, I'm your father

--- express/assets/js/voice.js
+++ express/assets/js/voice.js
@@ -1,6 +1,6 @@
 // Changer l'url pour pointer vers votre script RRFRemote.js
 
-var url = "http://localhost:3000/?cmd=";
+var url = "http://192.168.1.100:3000/?cmd=";
 
 // Ne rien changer en dessous...
 
