67de1bde7d6c64def3c67c2b70c5645da3b1e462
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 04:48:21 +0100

    Luke, I'm your father

--- RRFVoice.js
+++ RRFVoice.js
@@ -1,12 +1,7 @@
-const https = require('https');
+const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs');
-
-const privateKey  = fs.readFileSync('selfsigned.key', 'utf8');
-const certificate = fs.readFileSync('selfsigned.crt', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
 
 app.use(express.json());
 app.use(express.static("express"));
@@ -15,7 +10,7 @@ app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = https.createServer(credentials, app);
+const server = http.createServer(app);
 const port = 4000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
