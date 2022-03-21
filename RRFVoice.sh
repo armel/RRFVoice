f73f4c932c6dbb99cf974d3b989f4463d5ceabd9
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 16:24:27 +0100

    Improve packaging

--- RRFVoice.sh
+++ RRFVoice.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 case "$1" in
     start)
-        nohup node RRFVoiceServer.js &>/dev/null
-        nohup node RRFVoiceClient.js &>/dev/null
+        nohup node RRFVoiceServer.js & > /dev/null 2>&1
+        nohup node RRFVoiceClient.js & > /dev/null 2>&1
         ;;
     stop)
         search="node RRFVoice"
