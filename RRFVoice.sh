5b8a330af7cdb1a2657ef97d8afbab8469739620
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 15:55:37 +0100

    Improve code

--- RRFVoice.sh
+++ RRFVoice.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-SERVER_SCRIPT='RRFVoiceServer.js'
-CLIENT_SCRIPT='RRFVoiceServer.js'
-
-
 case "$1" in
     start)
         nohup node RRFVoiceServer.js &
         nohup node RRFVoiceClient.js &
         ;;
     stop)
-        search="node ${SERVER_SCRIPT}"
-        pkill -f "${search}"
-        search="node ${CLIENT_SCRIPT}"
+        search="node RRFVoice"
         pkill -f "${search}"
         ;;
 esac
