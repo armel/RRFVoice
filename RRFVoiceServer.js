017b25ef9d670d93ad84f5f60491366cb4693ade
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 16:59:37 +0100

    Improve packaging

--- RRFVoiceServer.js
+++ RRFVoiceServer.js
@@ -149,8 +149,8 @@ const requestHandler = (request, response) => {
 
 
 const options = {
-  key: fs.readFileSync('spotnik.f5nlg.ovh+3-key.pem'),
-  cert: fs.readFileSync('spotnik.f5nlg.ovh+3.pem),
+  key: fs.readFileSync('localhost+2-key.pem'),
+  cert: fs.readFileSync('localhost+2.pem'),
 };
 
 const server = https.createServer(options, requestHandler);
