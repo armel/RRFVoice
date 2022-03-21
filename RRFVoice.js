5bde0e19c680c352ffbf872cf1f375dae1c0e80c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-21 04:23:59 +0100

    Luke, I'm your father

--- RRFVoice.js
+++ RRFVoice.js
@@ -9,7 +9,7 @@ app.use('/', function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
   });
-const server = http.createServer(app);
+const server = https.createServer(app);
 const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
