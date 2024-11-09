0a8bf6cb5b2a5fa378afab0ca86902a00c533a0c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2024-11-09 16:27:10 +0100

    Add IDF

--- express/assets/js/voice.js
+++ express/assets/js/voice.js
@@ -48,6 +48,10 @@ function handleResult({ results }) {
     captured = "Salon Régional";
     httpGet(103, captured);
   }
+  else if(captured.includes("ile de france")) {
+    captured = "Salon ile de france";
+    httpGet(104, captured);
+  }
   else if(captured.includes("raptor")) {
     captured = "Raptor";
     httpGet(200, captured);
